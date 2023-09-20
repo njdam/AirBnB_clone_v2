56e6214a8cc898d6884e5354d916add68313b223
njdam <jeandamngaboyimana@gmail.com>
2023-09-20 20:30:14 +0000

    Nice Code 22h30.

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+
 import uuid
 from datetime import datetime
-from models import storage
+from sqlalchemy import Column, String, DATETIME
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, unique=True, nullable=False)
+    created_at = Column(DATETIME, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DATETIME, default=datetime.utcnow(), nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -29,8 +36,14 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+
+    def delete(self):
+        """A function for deleting a model instance from the storage"""
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
