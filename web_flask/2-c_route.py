fa54761ce17a9f590fc747c18337a07b18ec3be0
njdam <jeandamngaboyimana@gmail.com>
2023-10-21 18:27:25 +0000

    2. C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,21 +5,22 @@ from flask import Flask
 
 # Flask web application
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """A flask app to return Hello HBNB!"""
     return ("Hello HBNB!")
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hbnb():
     """a flsk app to return HBNB"""
     return ("HBNB")
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_page(text):
     """A flask app to return c is fun and replacing _ with space."""
     return ("c {}".format(text.replace('_', ' ')))
