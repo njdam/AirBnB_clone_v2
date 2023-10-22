2710564f38a2be4b5c1100781f8ed63f312fb5fd
njdam <jeandamngaboyimana@gmail.com>
2023-10-22 18:56:30 +0000

    8. List of states.

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A script to start a Flask web application #8. List of states"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+"""The Flask application strict slashes."""
+app.url_map.strict_slashes = False
+
+
+@app.route('/states_list')
+def states_list():
+    """Displaying List of States page."""
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda state: state.name)
+    for state in all_states:
+        state.cities.sort(key=lambda state: state.name)
+        pdict = {'states': all_states}
+        return (render_template('8-cities_by_states.html', **pdict))
+
+
+@app.teardown_appcontext
+def teardown_flask(exception):
+    """After each request you must remove the current SQLAlchemy Session."""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
