f69ebda9f6588b8fadaaea8eed7fe35fda5b9ce2
njdam <jeandamngaboyimana@gmail.com>
2023-10-25 22:40:44 +0000

    10. States and State.

---
+++ web_flask/9-states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""A simple Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def states(id=None):
+    """The states page"""
+    states = None
+    state = None
+    all_states = list(storage.all(State).values())
+    case = 404
+    if id is not None:
+        result = list(filter(lambda x: x.id == id, all_states))
+        if len(result) > 0:
+            state = result[0]
+            state.cities.sort(key=lambda x: x.name)
+            case = 2
+    else:
+        states = all_states
+        for state in states:
+            state.cities.sort(key=lambda x: x.name)
+        states.sort(key=lambda x: x.name)
+        case = 1
+    pdict = {
+        'states': states,
+        'state': state,
+        'case': case
+    }
+    return render_template('9-states.html', **pdict)
+
+
+@app.teardown_appcontext
+def flask_teardown(exc):
+    '''Flask app/request for closing storage.'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
